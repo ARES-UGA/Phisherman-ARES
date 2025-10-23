9495f54623644eb1f37ecf1f1d30b7d711f712df
Agni Athreya <agniathreya@gmail.com>
2025-10-23 12:29:43 -0400

    Save current working state before fixing Angular dev server integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,46 @@
-/* eslint-disable no-undef */
-
 const devCerts = require("office-addin-dev-certs");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-const urlDev = "https://localhost:3000/";
-const urlProd = "https://officedev.github.io/Office-Add-in-samples/Samples/hello-world/outlook-hello-world";
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
   return { ca: httpsOptions.ca, key: httpsOptions.key, cert: httpsOptions.cert };
 }
 
-module.exports = async (env, options) => {
-  const dev = options.mode === "development";
-  const config = {
-    devtool: "source-map",
-    entry: {
-      polyfill: ["core-js/stable", "regenerator-runtime/runtime"],
-      taskpane: ["./taskpane.html"],
-    },
-    output: {
-      clean: true,
-    },
-    resolve: {
-      extensions: [".html", ".js"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-          },
-        },
-        {
-          test: /\.html$/,
-          exclude: /node_modules/,
-          use: "html-loader",
-        },
-        {
-          test: /\.(png|jpg|jpeg|gif|ico)$/,
-          type: "asset/resource",
-          generator: {
-            filename: "assets/[name][ext][query]",
-          },
-        },
-      ],
-    },
+module.exports = async (env, argv) => {
+  const dev = argv.mode === "development";
+  const httpsOptions = await getHttpsOptions();
+
+  return {
+    entry: "./taskpane.html", // just bundle your HTML, not Angular
+    output: { clean: true },
     plugins: [
-      new HtmlWebpackPlugin({
-        filename: "taskpane.html",
-        template: "./taskpane.html",
-        chunks: ["polyfill", "taskpane"],
-      }),
+      new HtmlWebpackPlugin({ template: "./taskpane.html" }),
       new CopyWebpackPlugin({
         patterns: [
-          {
-            from: "assets/*",
-            to: "assets/[name][ext][query]",
-          },
-          {
-            from: "manifest*.*",
-            to: "[name]" + "[ext]",
-            transform(content) {
-              if (dev) {
-                return content;
-              } else {
-                return content.toString().replace(new RegExp(urlDev, "g"), urlProd);
-              }
-            },
-          },
+          { from: "assets", to: "assets" },
+          { from: "manifest*.xml", to: "[name][ext]" },
         ],
       }),
     ],
     devServer: {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
+      port: 3000,
+      static: ["."],
+      headers: { "Access-Control-Allow-Origin": "*" },
       server: {
         type: "https",
-        options: env.WEBPACK_BUILD || options.https !== undefined ? options.https : await getHttpsOptions(),
+        options: httpsOptions,
       },
-      port: process.env.npm_package_config_dev_server_port || 3000,
+      hot: true,
+    },
+    resolve: { extensions: [".js", ".html"] },
+    module: {
+      rules: [
+        { test: /\.html$/, use: "html-loader" },
+        { test: /\.(png|jpg|jpeg|gif|ico)$/, type: "asset/resource" },
+      ],
     },
   };
-
-  return config;
 };
+
